7355d435318debcfa4aeca6ee3683aa74d4c398c
enrstudio <enr.studio@gmaio.com>
2022-10-21 21:26:18 +0200

    audio fist

--- maze.js
+++ maze.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
    $("#win-link").hover(function(){
+     var context = new AudioContext();
+     // One-liner to resume playback when user interacted with the page.
+     context.resume().then(() => {
+       document.getElementById("toxic").play();
     $('.scary').addClass('appear');
     // $('.buddy').find('.status').remove();
-    var context = new AudioContext();
-    // One-liner to resume playback when user interacted with the page.
-    context.resume().then(() => {
-      document.getElementById("toxic").play();
     });
   });
 
